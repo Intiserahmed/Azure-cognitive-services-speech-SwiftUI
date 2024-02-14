e61f787d360fc250da80eeda540eae7859c5a42e
Intiser Ahmed <57252940+Intiserahmed@users.noreply.github.com>
2024-02-14 09:00:39 +0800

    Initial Commit

---
+++ Azure-cognitive-services-speech-SwiftUI/Azure_cognitive_services_speech_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Azure_cognitive_services_speech_SwiftUIApp.swift
+//  Azure-cognitive-services-speech-SwiftUI
+//
+//  Created by intiser Ahmed on 14/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Azure_cognitive_services_speech_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Azure-cognitive-services-speech-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Azure-cognitive-services-speech-SwiftUI
+//
+//  Created by intiser Ahmed on 14/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
